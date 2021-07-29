5767a411385f3addd1b3bdd7304fdbd112299954
borajapa <66993823+borajapa@users.noreply.github.com>
2021-07-29 16:07:00 -0300

    add manydeletes

--- src/graphql/modules/posts/resolvers.js
+++ src/graphql/modules/posts/resolvers.js
@@ -1,16 +1,24 @@
 import Post from '../../../models/Post';
 
-export default {
+const postResolve = {
     Query: {
         posts: () => Post.find(),
-        post: (_, { id}) => Post.findById(id),
+        post: (_, { id }) => Post.findById(id),
     },
     Mutation: {
         createPost: (_, { data }) => Post.create(data),
-        updatePost: (_, { id, data }) => Post.findOneAndUpdate(id, data, {new: true}),
-        deletePost: async (_, { id}) => {
-            const deleted = await Post.findOneAndDelete(id);
+        updatePost: (_, { id, data }) => Post.findOneAndUpdate(id, data, { new: true }),
+        deletePost: async (_, { id }) => {
+                        const deleted = await Post.findOneAndDelete(id);
             return !!deleted;
+        },
+        deleteManyPost: async (_, { ids }) => {
+            ids.forEach(async (id) => {                
+                await postResolve.Mutation.deletePost(_, {id}).then(res => console.log(res));
+            })
+            return true;
         }
     },
 };
+
+export default postResolve;
