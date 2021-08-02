0eb186c823f4ddc220cd6c45ca107ae6eded19eb
Joao Henrique <jhk.ferreira@gmail.com>
2021-08-02 16:09:00 -0300

    criacao migration user

--- src/db/migrations/20210730152855-create-user.js
+++ src/db/migrations/20210730152855-create-user.js
@@ -9,7 +9,16 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(320),
+        allowNull:false
+      },
+      name: {
+        type: Sequelize.STRING(200),
+        allowNull:false
+      },
+      password: {
+        type: Sequelize.STRING(250),
+        allowNull:false
       },
       createdAt: {
         allowNull: false,
