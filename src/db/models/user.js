c3507c9238ae7c56bcc1ed58e3d4c8e905a4ba6c
Joao Henrique <jhk.ferreira@gmail.com>
2021-08-02 16:17:40 -0300

    add model & bcryptjs

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,20 +1,45 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {    
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    freezeTableName: true,
-    timestamps: true,  
-    modelName: 'User',
-  });
-  return User;
+
+import {genSaltSync, hashSync, compareSync} from 'bcryptjs';
+
+module.exports = function (sequelize, DataTypes) {
+    var user = sequelize.define('user', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING(200),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(320),
+            allowNull: false
+        },        
+        password: {
+            type: DataTypes.STRING(250),
+            allowNull: true
+        }        
+    }, {
+        timestamps: false,
+        underscored: true,
+        freezeTableName: true,
+        hooks: {
+            beforeCreate: (user, options) => {
+                if (user.password) {
+                    const salt = genSaltSync();
+                    user.password = hashSync(user.password, salt);
+                }
+            },
+            beforeUpdate: (user, options) => {
+                if (user.changed('password')) {
+                    const salt = genSaltSync();
+                    user.password = hashSync(user.password, salt);
+                }
+            }
+        }
+    });
+    return user;
 };
